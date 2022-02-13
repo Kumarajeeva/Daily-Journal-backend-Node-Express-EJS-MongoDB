3adabb0865e7520f10e31a06459a4873244e9cbd
Kumarajeeva Elavarasan <kumarjeeva94@gmail.com>
2022-02-12 22:55:47 -0500

    App updated with MongoDB database

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Kumarajeeva:Mongodb29%40@cluster0.wzhai.mongodb.net/dailyJournalDB", {useNewUrlParser: true,useUnifiedTopology: true});
 
 const postSchema={
   title: String,
