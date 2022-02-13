c03b1fcb613bbd070bf6693c1b91d862c9b2e82d
Kumarajeeva Elavarasan <kumarjeeva94@gmail.com>
2022-02-12 22:38:04 -0500

    Initial Commit with local database

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose=require("mongoose");
+
+const homeStartingContent = "Contemporary climate change includes both global warming and its impacts on Earth's weather patterns. There have been previous periods of climate change, but the current changes are distinctly more rapid and not due to natural causes.Instead, they are caused by the emission of greenhouse gases, mostly carbon dioxide (CO2) and methane. Burning fossil fuels for energy use creates most of these emissions. Agriculture, steelmaking, cement production, and forest loss are additional sources.As human behavior and choices destabilize the Earth’s climate and biosphere, policymakers will need to do more. The shared nature of the threat also shows the need for closer and more comprehensive international cooperation to preserve the habitat in which human life has thrived.";
+const aboutContent = "This is a daily journal website with published contents about the happenings in the world. Users can compose or insert a new blog. The new composed blogs are published in the home page. Click compose and start writing new blogs!";
+const contactContent = "For enquires and doubts, reach out to us though email at abc123@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema={
+  title: String,
+  content: String
+};
+
+const Post=mongoose.model("Post", postSchema);
+
+
+
+app.get("/", function(req, res){
+
+  Post.find({},function(err,posts){
+    res.render("home", {startingContent: homeStartingContent, posts: posts});
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    } else {console.log(err)};
+  });
+
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err,posts){
+    res.render("post", {title: posts.title, content: posts.content });
+  });
+
+});
+
+let port=process.env.PORT;
+if(port==null||port==""){
+  port=3000;
+}
+
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
+});
